062cfdef56395237ff197e833c5a8b23fc21c776
Vladyslav Charuk <vlad.charu@gmail.com>
2024-07-26 20:14:01 +0300

    public folder added

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -3,14 +3,14 @@ const nodeExternals = require('webpack-node-externals');
 const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
 const {
-    NODE_ENV = 'produ',
+    NODE_ENV = 'prod',
 } = process.env;
 
 module.exports = {
     entry: './src/app.ts',
     mode: NODE_ENV,
     target: 'node',
-    watch: NODE_ENV === 'development',
+    watch: NODE_ENV === 'dev',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.js'
